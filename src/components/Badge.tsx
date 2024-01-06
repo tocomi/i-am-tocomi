e2ba31c55b2a32466a06f66efcea0be6f4027bd6
tocomi <tocomi0112@gmail.com>
2024-01-07 00:42:51 +0900

    feat: change font size

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,5 +1,5 @@
 export const Badge = ({ children }: { children: React.ReactNode }) => (
-  <span className="rounded-md bg-gray-500 px-2 py-1 text-gray-200">
+  <span className="rounded-md bg-gray-500 px-2 py-1 text-sm text-gray-200">
     {children}
   </span>
 )
