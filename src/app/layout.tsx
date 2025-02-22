050c50eb06b549bb893c0ec06b064b8fef7dd13c
tocomi <tocomi0112@gmail.com>
2025-02-22 23:46:41 +0900

    feat: use view transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { CustomLink } from '@/components'
 import type { Metadata } from 'next'
 import { Instrument_Sans } from 'next/font/google'
 import './globals.css'
+import { ViewTransitions } from 'next-view-transitions'
 
 const font = Instrument_Sans({
   subsets: ['latin'],
@@ -32,42 +33,44 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ja">
-      <body className={font.className}>
-        <div className="flex items-center justify-center sm:h-screen">
-          <main className="flex max-w-[800px] flex-col justify-between gap-12 p-8 sm:flex-row sm:gap-8">
-            <div className="flex max-w-[480px] flex-col items-center justify-between md:w-[480px]">
-              {children}
-            </div>
-            <div className="flex min-w-44 flex-1 flex-col items-start justify-center gap-8">
-              <div className="flex flex-col gap-2">
-                <h3 className="mb-1 font-bold text-2xl">Projects</h3>
-                <CustomLink href="/projects/oogiri">oogiri-bot</CustomLink>
-                <CustomLink href="/projects/oogiri-history">
-                  oogiri-history
-                </CustomLink>
-                <CustomLink href="/projects/umaaji-analyzer">
-                  umaaji-analyzer
-                </CustomLink>
-                <CustomLink href="/projects/koedame">koedame</CustomLink>
+    <ViewTransitions>
+      <html lang="ja">
+        <body className={font.className}>
+          <div className="flex items-center justify-center sm:h-screen">
+            <main className="flex max-w-[800px] flex-col justify-between gap-12 p-8 sm:flex-row sm:gap-8">
+              <div className="flex max-w-[480px] flex-col items-center justify-between md:w-[480px]">
+                {children}
               </div>
-              <div className="flex flex-col gap-2">
-                <h3 className="mb-1 font-bold text-2xl">Links</h3>
-                <CustomLink href="https://i-am-tocomi.super.site" external>
-                  Blog
-                </CustomLink>
-                <CustomLink href="https://github.com/tocomi" external>
-                  GitHub
-                </CustomLink>
-                <CustomLink href="https://twitter.com/tocomi0112" external>
-                  Twitter
-                </CustomLink>
+              <div className="flex min-w-44 flex-1 flex-col items-start justify-center gap-8">
+                <div className="flex flex-col gap-2">
+                  <h3 className="mb-1 font-bold text-2xl">Projects</h3>
+                  <CustomLink href="/projects/oogiri">oogiri-bot</CustomLink>
+                  <CustomLink href="/projects/oogiri-history">
+                    oogiri-history
+                  </CustomLink>
+                  <CustomLink href="/projects/umaaji-analyzer">
+                    umaaji-analyzer
+                  </CustomLink>
+                  <CustomLink href="/projects/koedame">koedame</CustomLink>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h3 className="mb-1 font-bold text-2xl">Links</h3>
+                  <CustomLink href="https://i-am-tocomi.super.site" external>
+                    Blog
+                  </CustomLink>
+                  <CustomLink href="https://github.com/tocomi" external>
+                    GitHub
+                  </CustomLink>
+                  <CustomLink href="https://twitter.com/tocomi0112" external>
+                    Twitter
+                  </CustomLink>
+                </div>
+                <CustomLink href="/">Home</CustomLink>
               </div>
-              <CustomLink href="/">Home</CustomLink>
-            </div>
-          </main>
-        </div>
-      </body>
-    </html>
+            </main>
+          </div>
+        </body>
+      </html>
+    </ViewTransitions>
   )
 }
