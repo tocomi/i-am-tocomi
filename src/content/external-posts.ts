fab816a20e5ced4d10023dd0353cf5401a057e06
tocomi <tocomi0112@gmail.com>
2025-10-06 02:22:48 +0900

    feat: hatena

--- src/content/external-posts.ts
+++ src/content/external-posts.ts
@@ -4,12 +4,22 @@ export type Platform = 'zenn' | 'hatena'
 
 export const externalPosts: Blog[] = [
   {
+    title:
+      '「フロントエンドカンファレンス北海道」で実施したクイズの中身を大公開！',
+    pubDate: new Date('2025-09-30'),
+    external: {
+      url: 'https://tech.techtouch.jp/entry/frontend-conference-hokkaido-2025',
+      platform: 'hatena',
+    },
+    tags: ['イベント'],
+  },
+  {
     title: 'Next.js のアプリに NextAuth でシンプルな認証機能をつける',
     pubDate: new Date('2024-02-14'),
     external: {
       url: 'https://zenn.dev/tocomi/articles/965c6ccb676a7b',
       platform: 'zenn',
     },
-    tags: ['Development'],
+    tags: ['技術'],
   },
 ]
--- src/content/types.ts
+++ src/content/types.ts
@@ -1,9 +1,11 @@
 import { z } from 'astro:content'
 
+export const tag = z.enum(['技術', 'イベント', '読書', '趣味', '日常'])
+
 export const blogSchema = z.object({
   title: z.string(),
   pubDate: z.date(),
-  tags: z.array(z.string()),
+  tags: z.array(tag),
   external: z
     .object({
       url: z.string(),
