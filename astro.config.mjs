9c0760e97957758d4fd0d633fbcf494f2fc98304
tocomi <tocomi0112@gmail.com>
2025-10-30 23:35:44 +0900

    fix: ogp url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://i-am-tocomi.pages.dev/',
   vite: {
     plugins: [tailwindcss()],
   },
